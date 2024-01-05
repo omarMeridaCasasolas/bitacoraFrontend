bca1d8e3b61aba84f2384a487cd871a86c2cd190
omarMeridaCasasolas <38450818+omarMeridaCasasolas@users.noreply.github.com>
2024-01-05 02:02:06 -0400

    Add files via upload

---
+++ src/app/components/shared/modal-asignar-unidad/modal-asignar-unidad.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-asignar-unidad',
+  templateUrl: './modal-asignar-unidad.component.html',
+  styleUrls: ['./modal-asignar-unidad.component.css']
+})
+export class ModalAsignarUnidadComponent {
+
+}
---
+++ src/app/components/shared/modal-editar-administrador/modal-editar-administrador.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-editar-administrador',
+  templateUrl: './modal-editar-administrador.component.html',
+  styleUrls: ['./modal-editar-administrador.component.css']
+})
+export class ModalEditarAdministradorComponent {
+
+}
